05c149ca146b8255a96e2c7f4a6ef7833c0bb532
Lorenzo Pernigoni <l.pernigoni@outlook.it>
2022-04-07 10:17:59 +0200

    Add files via upload

---
+++ OCaml/Interprete_scoping_dinamico/test_interprete_scoping_dinamico.ml
@@ -0,0 +1,32 @@
+open Interprete_scoping_dinamico;;
+
+(*  ocamlc interprete_scoping_dinamico.ml
+    ocamlc interprete_scoping_dinamico.cmo test_interprete_scoping_dinamico.ml 
+    ocaml
+
+    #load "interprete_scoping_dinamico.cmo";;
+    #use "test_interprete_scoping_dinamico.ml";;
+
+    ^C
+
+    del *.cmo, *.cmi, *.exe     *)
+
+eval (Let ("x", EInt 1, 
+        Let ("f", Fun ("y", Add (Den "x", Den "y")), 
+            Let ("x", EInt 10, Apply (Den "f", EInt 3))))) empty_env;;
+(* - : evT = Int 13 (scoping dinamico) *)
+
+eval (Let ("x", EInt 1, 
+        Let ("f", DFun ("y", Add (Den "x", Den "y"), [EInt 1; EInt 2; EInt 3]),
+            Let ("x", EInt 10, Apply (Den "f", EInt 3))))) empty_env;;
+(* - : evT = Int 13 *)
+
+eval (Let ("x", EInt 1, 
+        Let ("f", ParamsFun (["y"; "z"], Add (Den "x", Den "y")), 
+            Let ("x", EInt 10, ApplyParams (Den "f", [EInt 3; EInt 1000]))))) empty_env;;
+(* - : evT = Int 13 *)
+
+eval (Let ("x", EInt 1, 
+        Let ("f", Fun ("y", Add (Den "x", Den "y")), 
+            Let ("x", EInt 10, Iterate (Den "f", EInt 3, EInt 10))))) empty_env;;
+(* - : evT = Int 103 *)
